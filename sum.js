630b321fdfda71bb2a897264962d3bb3d87b3576
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-13 23:50:51 +0600

    added sum file

---
+++ sum.js
@@ -0,0 +1,24 @@
+// var numbers = [54, 35, 85, 74, 87, 23, 5];
+// for (i = 0; i < numbers.length; i++) {
+//     var element = numbers[i];
+//     console.log(element);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+var numbers = [51, 65, 84, 69, 56, 45,];
+for (i = 0; i <= numbers.length; i++) {
+    const element = numbers[i];
+    console.log(element);
+
+}
