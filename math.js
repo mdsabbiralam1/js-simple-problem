406d0100a3da163ecb7df77595a842abbbe21b17
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-13 10:55:48 +0600

    added math file

--- math.js
+++ math.js
@@ -1,1 +1,13 @@
-d
+// const myNumber = -3;
+// const output = Math.abs(myNumber);
+
+const myNumber = 25.59;
+// const output = Math.ceil(myNumber);  // dosomik er porer sonhkha habe.
+// const output = Math.floor(myNumber); // dosomik er por niche jabe
+// const output = Math.round(myNumber); //dosomik er kasakasi songkha hobe.
+
+for (let i = 0; i <= 20; i++) {
+    const output = Math.random() * 6;
+    const rounded = Math.round(output);
+    console.log(rounded);
+}
