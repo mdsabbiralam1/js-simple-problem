77e61422986462a86ae68b06c5e8219c25c837e2
mdsabbiralam1 <mdsabbiralam009@gmail.com>
2021-08-13 11:30:32 +0600

    added swap file

--- swap.js
+++ swap.js
@@ -1,1 +1,12 @@
-bs
+var first = 5;
+var second = 7;
+console.log(first, second);
+// first approach
+// var temp = first;
+// first = second;
+// second = temp;
+// console.log(first, second);
+
+// destructuting
+[first, second] = [second, first];
+console.log(first, second);
